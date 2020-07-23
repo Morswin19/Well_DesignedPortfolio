bf7d4728c417d77e771e2810225a954008f89358
Morswin19 <piotr.kalman85@gmail.com>
2020-07-23 20:23:38 +0200

    Updates

---
+++ precache-manifest.d644cef73958e0e4c28ae204ddb2cf64.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f01dd59f83213362b16d1a6d6fcb0d0",
+    "url": "/Well_DesignedPortfolio/index.html"
+  },
+  {
+    "revision": "ff75b81f68ac77698afe",
+    "url": "/Well_DesignedPortfolio/static/css/main.5600dc18.chunk.css"
+  },
+  {
+    "revision": "074a94e5c7db033afb7e",
+    "url": "/Well_DesignedPortfolio/static/js/2.c1ce3c10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Well_DesignedPortfolio/static/js/2.c1ce3c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff75b81f68ac77698afe",
+    "url": "/Well_DesignedPortfolio/static/js/main.1090802e.chunk.js"
+  },
+  {
+    "revision": "8eed41ef14ea788bb714",
+    "url": "/Well_DesignedPortfolio/static/js/runtime-main.c210389c.js"
+  },
+  {
+    "revision": "f6e3a2eaadc48fa15d5edca5a16587ee",
+    "url": "/Well_DesignedPortfolio/static/media/10.f6e3a2ea.jpg"
+  },
+  {
+    "revision": "6d5ed434ce4769ed16168cecc0b85efe",
+    "url": "/Well_DesignedPortfolio/static/media/2.6d5ed434.jpg"
+  },
+  {
+    "revision": "7f2a931f9db8e6779db4953b3d86b901",
+    "url": "/Well_DesignedPortfolio/static/media/3.7f2a931f.jpg"
+  },
+  {
+    "revision": "76e6036f13555d3bc857f147ec6b89e7",
+    "url": "/Well_DesignedPortfolio/static/media/4.76e6036f.jpg"
+  },
+  {
+    "revision": "7f17a544c8541fe4bff94aad15696149",
+    "url": "/Well_DesignedPortfolio/static/media/5.7f17a544.jpg"
+  },
+  {
+    "revision": "481f714925817b8806aa3fa0d682587c",
+    "url": "/Well_DesignedPortfolio/static/media/6.481f7149.jpg"
+  },
+  {
+    "revision": "e0998e9f481182098aad1441c2402d64",
+    "url": "/Well_DesignedPortfolio/static/media/7.e0998e9f.jpg"
+  },
+  {
+    "revision": "59681db08b71459b509c005a98d60b82",
+    "url": "/Well_DesignedPortfolio/static/media/8.59681db0.jpg"
+  },
+  {
+    "revision": "76208fb468c6f8f9958704ab5f7a11d9",
+    "url": "/Well_DesignedPortfolio/static/media/9.76208fb4.jpg"
+  }
+]);
